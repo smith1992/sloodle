f075eb7869c3fd944cf0a9a01072aec3181a7534
edmund.edgar <edmund.edgar@a6645870-f120-0410-93b9-49b3dcf19678>
2006-11-03 15:24:17 +0000

    Removed spurious "requires" line - should allow installation on earlier Moodle versios.

--- sloodle/version.php
+++ sloodle/version.php
@@ -5,8 +5,8 @@
 ///  This fragment is called by moodle_needs_upgrading() and /admin/index.php
 /////////////////////////////////////////////////////////////////////////////////
 
-$module->version  = 2006100705;  // change only last two digits in 1.6.x!!
-$module->requires = 2006050512;   // The version of Moodle that is required
+$module->version  = 2006100701;  // change only last two digits in 1.6.x!!
+//$module->requires = 2006050512;   // The version of Moodle that is required
 $module->cron     = 0;            // How often should cron check this module (seconds)?
 
 ?>
