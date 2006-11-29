afa610080542d98abda504156daf4c50330a9f70
edmund.edgar <edmund.edgar@a6645870-f120-0410-93b9-49b3dcf19678>
2006-11-29 12:52:24 +0000

    Removed non-admin access to scripts

--- sloodle/login/sl_signup_setup.php
+++ sloodle/login/sl_signup_setup.php
@@ -15,9 +15,10 @@
 	if (isadmin()) {
 		print_simple_box('You need to tell your prim to use the following URL to talk to Moodle:<br />'.$pasteurl, "center");
 	} else {
-		//print_simple_box('You need admin privileges to access this page.', "center");
-		print_simple_box('You would normally need admin privileges to access this page, but I\'ll let you in, since it\'s a demo.', "center");
-		print_simple_box('You need to tell your prim to use the following URL to talk to Moodle:<br />'.$pasteurl, "center");
+		print_simple_box('You need admin privileges to access this page.', "center");
+		//print_simple_box('You would normally need admin privileges to access this page, but I\'ll let you in, since it\'s a demo.', "center");
+		//print_simple_box('You need to tell your prim to use the following URL to talk to Moodle:<br />'.$pasteurl, "center");
+		exit;
 	}
 
 	$subs = array(
