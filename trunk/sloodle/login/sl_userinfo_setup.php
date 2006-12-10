351e9ec3b9545f33cad532c7525ac5aed86a2624
edmund.edgar <edmund.edgar@a6645870-f120-0410-93b9-49b3dcf19678>
2006-12-10 23:41:11 +0000

    Removed redundant prim password info

--- trunk/sloodle/login/sl_userinfo_setup.php
+++ trunk/sloodle/login/sl_userinfo_setup.php
@@ -12,7 +12,7 @@
 	$pasteurl = $userinfourl.'?pwd='.sloodle_prim_password();
 
 	if (isadmin()) {
-		print_simple_box('You need to tell your prim to use the following URL to talk to Moodle:<br />'.$pasteurl, "center");
+		//print_simple_box('You need to tell your prim to use the following URL to talk to Moodle:<br />'.$pasteurl, "center");
 	} else {
 		print_simple_box('You need admin privileges to access this page.', "center");
 		//print_simple_box('You would normally need admin privileges to access this page, but I\'ll let you in, since it\'s a demo.', "center");
