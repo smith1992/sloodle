8b474abb9fddf9725c8cbbd125502ba091486f5d
daniel.livingstone <daniel.livingstone@a6645870-f120-0410-93b9-49b3dcf19678>
2007-01-18 22:37:16 +0000

    Trial of manual entry for SL avatar/Moodle user authentication

---
+++ trunk/sloodle/login/sl_loginzone_manual_entry.php
@@ -0,0 +1,43 @@
+<?php
+
+	require_once('../config.php');
+	require_once('../locallib.php');
+	require_once('sl_authlib.php');
+
+	$sloodleerrors = array();
+
+	$sloodleinstallurl = 'http://'.$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME'];
+
+	print_header('Enter your Second Life Avatar Details', '', '', '', false, '', '', false, '');
+	print_heading('Sloodle User to SL Avatar authentication page');
+
+//first verify that user is not a guest
+if (isguest()) {
+    error(get_string('noguestauthentication', 'sloodle'), $referrer);
+    exit;
+}
+
+if (!isloggedin()) {
+    error(get_string('not logged in', 'sloodle'), $referrer);
+    exit;
+}
+
+//print_header();
+
+    $userid = $USER->id;
+
+echo '<form name="input" action="sl_loginzone_manual_update.php" method="get">';
+echo 'Second Life avatar name: <input type="text" name="avname">';
+echo ' Second Life avatar UUID (key): <input type="text" name="uuid">';
+echo ' <input type="submit" value="Submit">';
+echo ' </form>';
+
+//$options = array(0 => "avname", 1 => "uuid");
+//print_single_button('/mod/sloodle/login/sl_loginzone_manual_entry.php', $options, 'Submit', 'get', '_self');
+
+print_footer();
+
+	exit;
+
+
+?>
