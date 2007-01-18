ddca0c17e4d748bd96a2d32a05b0056135628914
daniel.livingstone <daniel.livingstone@a6645870-f120-0410-93b9-49b3dcf19678>
2007-01-18 22:37:16 +0000

    Trial of manual entry for SL avatar/Moodle user authentication

---
+++ sloodle/login/sl_loginzone_manual_update.php
@@ -0,0 +1,54 @@
+<?php
+
+	require_once('../config.php');
+	require_once('../locallib.php');
+	require_once('sl_authlib.php');
+
+	$sloodleerrors = array();
+
+	print_header('Second Life Avatar Details', '', '', '', false, '', '', false, '');
+	print_heading('Sloodle User to SL Avatar Authentication ');
+
+//first verify that user is not a guest
+if (isguest()) {
+    error(get_string('noguestauthentication', 'sloodle'), $referrer);
+    exit;
+}
+
+if (!isloggedin()) {
+    error(get_string('not logged in', 'sloodle'), $referrer);
+    exit;
+}
+
+	$sl_uuid = required_param('uuid', PARAM_RAW);  //Pass the Subject to the Blog - PHP will not run if we don't get this.
+	$sl_avname = required_param('avname', PARAM_RAW);  // Pass the actual message - PHP will not run if we don't get this.
+
+
+	$sloodleuser = sloodle_get_sloodle_user_for_moodle_user($USER);
+	$isnewuser = ($sloodleuser == null);
+	if ($isnewuser) {
+		$sloodleuser = new object();
+		$sloodleuser->userid = $USER->id;
+		$sloodleuser->uuid = $sl_uuid;
+		$sloodleuser->avname = $sl_avname;
+	}
+//	if ( ($sloodleuser != null) && ($sloodleuser->avname != null) && ($sloodleuser->avname != '') ) {
+//		print_simple_box('Already got your login name!', "center");
+//		exit;
+//	} 
+	
+	$dbresult = false;
+	if ($isnewuser) {
+		$dbresult = insert_record('sloodle_users',$sloodleuser);	
+	} else {
+		$dbresult = update_record('sloodle_users',$sloodleuser);	
+	}
+	if (!$dbresult) {
+		print_simple_box('Sorry, something went wrong.', "center");
+		include('progressbar.html');
+		exit;
+	}
+	print_simple_box('Updated your SL avatar details!', "center");
+	print_footer();
+
+?>
