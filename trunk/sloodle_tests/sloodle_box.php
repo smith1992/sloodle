6ecb17cfddfca09812480ff258b78bfd6bfc90c0
edmund.edgar <edmund.edgar@a6645870-f120-0410-93b9-49b3dcf19678>
2007-04-09 03:45:02 +0000

    Added test for teacher course list

--- trunk/sloodle_tests/sloodle_box.php
+++ trunk/sloodle_tests/sloodle_box.php
@@ -6,5 +6,6 @@ require_once('lib/simpletest/reporter.php');
 $test = &new GroupTest('Sloodlebox Tests');
 $test->addTestFile('userinfo_test.php');
 $test->addTestFile('object_server_setup.php');
+$test->addTestFile('class_setup.php');
 exit ($test->run(new HtmlReporter()) ? 0 : 1);
 ?>
