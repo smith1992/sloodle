966e0e48f7ccc176e91f7a99ffc30fb088ae96ea
edmund.edgar <edmund.edgar@a6645870-f120-0410-93b9-49b3dcf19678>
2007-05-05 02:15:16 +0000

    Updated classroom_creator script to use XMLRPC to find out when an avatar has been authenticated and move on to the next step.

--- sloodle/login/sl_welcome_reg.php
+++ sloodle/login/sl_welcome_reg.php
@@ -62,7 +62,7 @@ print "<h3>sloodleuser</h3";
 	// If the object passed us a channel parameter, we'll use it to tell the object that the authentication is done.
 	// If not, the avatar will just have to touch the object again.
 	if ( ($channel != null) && ($channel != '') ) {
-	print "<h1>channel is :$channel:</h1>";
+		//print "<h1>channel is :$channel:</h1>";
 		flush();
 		$xmlrpcresult = sloodle_send_xmlrpc_message($channel,0,"OK|SLOODLE_AUTHENTICATION_DONE|".$sloodleuser->uuid);
 		if (!$xmlrpcresult) {
