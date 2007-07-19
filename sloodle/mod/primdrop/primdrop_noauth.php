78c0cd7860f4ae2d340efd3bf5d9b851199d80cb
jerkem <jerkem@a6645870-f120-0410-93b9-49b3dcf19678>
2007-07-19 22:26:00 +0000

    Need versions, LSL and schemas...

--- sloodle/mod/primdrop/primdrop_noauth.php
+++ sloodle/mod/primdrop/primdrop_noauth.php
@@ -1,5 +1,5 @@
 <?php  // $Id: insert.php,v 1.2 2005/02/07 20:33:43 skodak Exp $
-	// Edited by Jeremy Kemp for the Sloodle project.
+	//Edited by Jeremy Kemp for the Sloodle project.
 	//This file should sit in the www ROOT dir of you main Moodle course.
 
     require_once('../../../../config.php');
