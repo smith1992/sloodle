d6fdcea4e1641fde815b0ee4e260c745ab9d4969
pedro.mcmillan <pedro.mcmillan@a6645870-f120-0410-93b9-49b3dcf19678>
2007-10-17 23:01:32 +0000

    Updated chat linker script to allow unregistered avatars' chat to be logged using the Moodle guest account, and added temporary fix for issue 6 (name truncation).
    Also cleaned up coding standards and comments in the file.

--- sloodle/mod/chat/sl_user_chat_linker.php
+++ sloodle/mod/chat/sl_user_chat_linker.php
@@ -1,191 +1,113 @@
-<?php  // $Id: insert.php,v 1.2 2005/02/07 20:33:43 skodak Exp $
-	// Edited by Paul Andrews for the Sloodle project.
-	//This file should sit in the www ROOT dir of you main Moodle course.
+<?php
+// This script allows the Sloodle WebIntercom (aka "ChatCast" or "ChatLogger") tool in Second Life to interact with a Moodle chatroom
+// Part of the Sloodle Project (www.sloodle.org)
 
-//    require_once('../../../../config.php');
+// Many contributors, including...
+// Paul Andrews, Daniel Livingstone, Peter Bloomfield..... more?
 
 require_once('../../config.php');
 require_once('../../locallib.php');
 require_once('../../login/sl_authlib.php');
 
+// Get the standard chat library functions
 require_once($CFG->dirroot .'/mod/chat/lib.php');
 
-sloodle_prim_require_script_authentication(); // make sure the client that's talking to us is allowed to do so.
-//print "Moodle user ID is $sl_userid";
+// Make sure a valid Second Life item is talking to us
+sloodle_prim_require_script_authentication();
 
-	// This next bit is new - we want to pass the other chat params to Moodle via Second Life.
-	$sl_courseid = optional_param('courseid', 0, PARAM_INT);	
-	$sl_chatid = optional_param('chat_id', 0, PARAM_INT);  //Pass the ID of the Chat room to log the chat in.
+// We ought to be passed the ID's of the Moodle course and chatroom to which we are linking
+// If we are passed just a course ID, then the script will output a list of all chatrooms in the course
+// Otherwise, we are querying and/or writing to the chatroom
+$sl_courseid = optional_param('courseid', 0, PARAM_INT);	
+$sl_chatid = optional_param('chat_id', 0, PARAM_INT);
 
-	if ( ($sl_courseid == 0) && ($sl_chatid == 0) ) {
-		sloodle_prim_render_error('Course ID and Chat ID missing');
-		exit;
-	}
+// Make sure we have at least one of the course or chatroom ID parameters
+if ( ($sl_courseid == 0) && ($sl_chatid == 0) ) {
+    sloodle_prim_render_error('Course ID and Chat ID missing');
+    exit;
+}
 
-	if ($sl_chatid == 0) {
-		if (! $chatroomset = get_recordset("chat", "course", $sl_courseid) ) {
-			sloodle_prim_render_error('Could not find chat module');
-			exit;
-		}
-
-		$chatrooms = recordset_to_array($chatroomset);
-		if (count($chatrooms) == 0) {
-			sloodle_prim_render_error('No chatrooms found');
-			exit;
-		}
-
-		$data = array();
-		foreach($chatrooms as $cr) {
-			$data[] = array(
-				$cr->id,
-				$cr->name
-			);
-		}
-
-		sloodle_prim_render_output($data);
-		
-	}
-	// $sl_userid = required_param('user_id', PARAM_INT);  /*Pass the ID of the Chat Bot account into Moodle, this is the number at the END of the URL of the page showing the profile of the chatbot.  EG: http://www.sloodle.com/user/view.php?id=158 Thus ID for Sloodle.com Chatbot = 158. 
-	$chat_message = optional_param('chat_message','', PARAM_RAW); // PA: This has changed , the message is now optional as you may just want to VIEW the chat room...
-	
-
-    //if (!$chatuser = get_record('chat_users', 'userid', $sl_userid)) {
-     //   sloodle_prim_render_error('Not logged in!  Please log the chatbot INTO the chat room you wish to use.'); //PA: This Uses Moodle's built in Error handler - :o)
-	//}
-/// Add the message to the database - as long as the message isn't empty!
-//	echo $chat_message;
-    if (!empty($chat_message)) {
-
-		sloodle_prim_require_user_login(); // check the avatar name and/or uuid argument s, and log the user in (creating a $USER) variable, or return errors to the clie nt.  
-
-		$sl_userid = $USER->id;
-
-
-		$chat_message = addslashes(clean_text(stripslashes($chat_message), FORMAT_MOODLE));  // Strip bad tags 
-
-
-//      $message->chatid = $chatuser->chatid; PA: Old Moodle code.
-//		This creates an array for all the data.
-		$message->chatid = $sl_chatid; //Pass the ID of chat room into the database.
-		$message->userid = $sl_userid; //Pass the ID of the Chat Bot into the database.
-        $message->message = $chat_message; // Pass the message in.
-        $message->timestamp = time(); // Add the unix timestamp
-
-		/* PA: Notes on insert_record function - this lives in datalib.php
- 		* Insert a record into a table and return the "id" field if required
-		 *
-		 * If the return ID isn't required, then this just reports success as true/false.
-		 * $dataobject is an object containing needed data
-		 *
-		 * @uses $db
-		 * @uses $CFG
-		 * @param string $table The database table to be checked against.
-		 * @param array $dataobject A data object with values for one or more fields in the record
-		 * @param boolean $returnid Should the id of the newly created record entry be returned? If this option is not requested then true/false is returned.
-		 * @param string $primarykey The primary key of the table we are inserting into (almost always "id")
-
-		 function insert_record($table, $dataobject, $returnid=true, $primarykey='id')  */
-			
-			
-					if (!insert_record('chat_messages', $message)) {  //PA: So here $message is our array :o)
-						sloodle_prim_render_error('Could not insert a chat message!');
-					}
-			
-			//        $chatuser->lastmessageping = time() - 2;
-			//        update_record('chat_users', $chatuser);
-			
-			//        PA: This last bit updates Moodle's Global Logs - I've removed it for now...
-			//        if ($cm = get_coursemodule_from_instance('chat', $chat->id, $course->id)) {
-			//            add_to_log($course->id, 'chat', 'talk', "view.php?id=$cm->id", $chat->id, $cm->id);
-			//        }
+// Has the chatroom ID been omitted?
+if ($sl_chatid == 0) {
+    // Make sure there is a chatroom module
+    if (! $chatroomset = get_recordset("chat", "course", $sl_courseid) ) {
+        sloodle_prim_render_error('Could not find chat module');
+        exit;
     }
 
-/*
+    // Get a list of chatrooms
+    $chatrooms = recordset_to_array($chatroomset);
+    if (count($chatrooms) == 0) {
+        // Error - no chatrooms found in this course
+        sloodle_prim_render_error('No chatrooms found');
+        exit;
+    }
 
-Now we need to output the chat - unlike Moodle's "Real time" chat we only need to update the output each time this page is called by Second Life.
+    // Go through each chatroom to format the data correctly
+    $data = array();
+    foreach($chatrooms as $cr) {
+        $data[] = array(
+                        $cr->id,
+                        $cr->name
+                        );
+    }
 
-So first let's get the Chat messages for the chat room we are in using Moodle's built in datalib function "Get Records".
+    // Output the list of chatrooms
+    sloodle_prim_render_output($data);
+}
 
-/**
- * Get a number of records as an array of objects
- *
- * Can optionally be sorted eg "time ASC" or "time DESC"
- * If "fields" is specified, only those fields are returned
- * The "key" is the first column returned, eg usually "id"
- * limitfrom and limitnum must both be specified or not at all
- *
- * @uses $CFG
- * @param string $table The database table to be checked against.
-  * @param string $field The database field name to search
-  * @param string $value The value to search for in $field
-  * @param string $sort Sort order (as valid SQL sort parameter)
- * @param string $fields A comma separated list of fields to be returned from the chosen table.
- * @param int $limitfrom Return a subset of results starting at this value (*must* set $limitnum)
- * @param int $limitnum Return a subset of results, return this number (*must* set $limitfrom)
- * @return array|false Returns an array of found records (as objects) or false if no records or error occured.
+// Has a chat message been specified? (If not, then we are just viewing the chat room, and not sending any data to it)
+$chat_message = optional_param('chat_message','', PARAM_RAW);
+if (!empty($chat_message)) {
 
-EG function get_records($table, $field='', $value='', $sort='', $fields='*', $limitfrom='', $limitnum='')
+    // Initially login as the guest user
+    $guestinfo = get_guest();
+    $USER = get_complete_user_data('id',$guestinfo->id);
+    // Attempt to login the user from Sloodle
+    // (If this fails, then the guest user is the fall-back)
+    sloodle_prim_user_login();
 
- */
+    // Extract the user ID
+    $sl_userid = $USER->id;
+    // Make sure the chat message is 'safe'
+    $chat_message = addslashes(clean_text(stripslashes($chat_message), FORMAT_MOODLE));  // Strip bad tags 
+    
+    // Format our message data for a database record
+    $message->chatid = $sl_chatid; //Pass the ID of chat room into the database.
+    $message->userid = $sl_userid; //Pass the ID of the Chat Bot into the database.
+    $message->message = $chat_message; // Pass the message in.
+    $message->timestamp = time(); // Add the unix timestamp
+    // Insert the chat message into the database
+    if (!insert_record('chat_messages', $message)) {
+        // Something went wrong...
+        sloodle_prim_render_error('Could not insert a chat message!');
+    }
+}
 
-
-// Updated query!
-/*
-
-/**
- * Get a number of records as an array of objects
- *
- * The "key" is the first column returned, eg usually "id"
- * The sql statement is provided as a string.
- *
- * @uses $CFG
- * @uses $db
- * @param string $sql The SQL string you wish to be executed.
- * @return array|false Returns an array of found records (as objects) or false if no records or error occured.
- */
-//function get_records_sql($sql)
-
+// Construct an SQL query to get all recent chat messages from the database
 $time_delay = time() - 1*60;
-
-
 $query = 'SELECT mdl_chat_messages.id, mdl_chat_messages.chatid, mdl_chat_messages.userid, mdl_user.firstname, mdl_user.lastname, mdl_chat_messages.message, mdl_chat_messages.groupid, mdl_chat_messages.system, mdl_chat_messages.timestamp
 FROM mdl_chat_messages LEFT JOIN mdl_user ON mdl_chat_messages.userid = mdl_user.id
 WHERE (((mdl_chat_messages.chatid)='.$sl_chatid.') AND((mdl_chat_messages.timestamp)>='.$time_delay.')) ORDER BY mdl_chat_messages.timestamp DESC';
 
+// Obtain the resulting recordset as an array
 $rs = get_recordset_sql($query);
 $results = recordset_to_array($rs);
-//$res = mysql_query($query);
 
-//var_dump($results);
-//exit;
+// Make sure we got some results
 if ($results) {
-	foreach($results as $m) {
-		print "Line: ".$m->id . " ";
-		//Print "Chat ID: ".$messages['chatid'] . " ";
-		//Print "User ID: ".$messages['userid'] . " ";
-		print $m->firstname . " ".$m->lastname." ";
-		//Print "Lastname: ".$messages['lastname']. " ".$messages['lastname']." ";
-		print $m->message. " ";
-		//Print "Time Stamp: ".$messages['timestamp'] . " <br>";
-	}
+    // Go through each chat message
+    foreach($results as $m) {
+        // Output the message in the format "Line: <number> <firstname> <lastname> <message>
+        
+        //print "Line: ".$m->id . " "; // We would like to do this...
+        // ... but currently (2007-10-17) the in-world chat logger script expects the line number to be exactly 4 digits long, so format it as such:
+        print "Line: " . sprintf("%04d", $m->id) . " ";
+        
+        print $m->firstname . " ".$m->lastname." ";
+        print $m->message. " ";
+    }
 }
 exit;
-//$messages = mysql_fetch_array($res); No good - just gives me the 1st result...!
-
-// Lets get all results...
-
-while($messages = mysql_fetch_array($res))
-{
-print "Line: ".$messages['id'] . " ";
-//Print "Chat ID: ".$messages['chatid'] . " ";
-//Print "User ID: ".$messages['userid'] . " ";
-print $messages['firstname'] . " ".$messages['lastname']." ";
-//Print "Lastname: ".$messages['lastname']. " ".$messages['lastname']." ";
-print $messages['message']. " ";
-//Print "Time Stamp: ".$messages['timestamp'] . " <br>";
-
-
-
-}
 
 ?>
