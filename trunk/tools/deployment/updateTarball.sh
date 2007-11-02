b77b921d676d8741463eb7fcab315b03daddb5db
edmund.edgar <edmund.edgar@a6645870-f120-0410-93b9-49b3dcf19678>
2007-11-02 11:21:03 +0000

    Added check to see if revision number has changed

--- trunk/tools/deployment/updateTarball.sh
+++ trunk/tools/deployment/updateTarball.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-# 
+#
 # Script to generate tarballs / zip files from SVN repository at Google.
-# Expects the latest sloodle module directory to have been checked out once into SVNROOT.
+# Expects the latest sloodle module directory to have been checked out once into                                              SVNROOT.
 # Will update itself.
-# Creates one tarball / zip per revision, and copies the latest to sloodle.tar.gz / sloodle.zip
+# Creates one tarball / zip per revision, and copies the latest to sloodle.tar.g                                             z / sloodle.zip
 # (C) Edmund Edgar, 2007-09-09
 # Licensed under the GPL v2 - see sloodle/COPYING for details.
 
@@ -23,20 +23,27 @@ CP=/bin/cp
 SED=/bin/sed
 ECHO=/bin/echo
 ZIP=/usr/bin/zip
+CAT=/bin/cat
 
-REVISION=`${SVN} update ${SVNROOT} | ${GREP} 'At revision' | ${AWK} '{print $3}' | $SED s/\\\\.//`
+OLDREVISION=`${CAT} ${CODEROOT}/sloodle/REVISION`
+REVISION=`${SVN} update ${SVNROOT} | ${GREP} 'At revision' | ${AWK} '{print $3}'                                              | $SED s/\\\\.//`
 REVISION=r${REVISION}
+echo $REVISION
 
-${RSYNC} -arvz --delete ${SVNROOT}/ ${CODEROOT}/sloodle/ --exclude=".svn"
-$ECHO ${REVISION} > ${CODEROOT}/sloodle/REVISION
+if [[ ${OLDREVISION} != ${REVISION} ]]; then
 
-cd ${CODEROOT}
-${TAR} zcvf ${WEBROOT}/sloodle.${REVISION}.tar.gz sloodle
-${CP} ${WEBROOT}/sloodle.${REVISION}.tar.gz ${WEBROOT}/sloodle.tar.gz.temp
-${MV} ${WEBROOT}/sloodle.tar.gz.temp ${WEBROOT}/sloodle.tar.gz
-#echo "${TAR} zcvf ${WEBROOT}/sloodle.${REVISION}tar.gz . -C ${CODEROOT}"
+        ${RSYNC} -arvz --delete ${SVNROOT}/ ${CODEROOT}/sloodle/ --exclude=".svn                                             "
+        $ECHO ${REVISION} > ${CODEROOT}/sloodle/REVISION
 
-${ZIP} ${WEBROOT}/sloodle.${REVISION}.zip -r sloodle/
-${CP} ${WEBROOT}/sloodle.${REVISION}.zip ${WEBROOT}/sloodle.zip.temp
-${MV} ${WEBROOT}/sloodle.zip.temp ${WEBROOT}/sloodle.zip
+        cd ${CODEROOT}
+        ${TAR} zcvf ${WEBROOT}/sloodle.${REVISION}.tar.gz sloodle
+        ${CP} ${WEBROOT}/sloodle.${REVISION}.tar.gz ${WEBROOT}/sloodle.tar.gz.te                                             mp
+        ${MV} ${WEBROOT}/sloodle.tar.gz.temp ${WEBROOT}/sloodle.tar.gz
+        #echo "${TAR} zcvf ${WEBROOT}/sloodle.${REVISION}tar.gz . -C ${CODEROOT}                                             "
+
+        ${ZIP} ${WEBROOT}/sloodle.${REVISION}.zip -r sloodle/
+        ${CP} ${WEBROOT}/sloodle.${REVISION}.zip ${WEBROOT}/sloodle.zip.temp
+        ${MV} ${WEBROOT}/sloodle.zip.temp ${WEBROOT}/sloodle.zip
+
+fi
 
