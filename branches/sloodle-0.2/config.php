5d143000cade9c42754faa1e7ab1e7a7ccc2c11f
pedro.mcmillan <pedro.mcmillan@a6645870-f120-0410-93b9-49b3dcf19678>
2007-11-29 12:19:19 +0000

    Fixed another minor bug affecting module configuration.

--- branches/sloodle-0.2/config.php
+++ branches/sloodle-0.2/config.php
@@ -2,7 +2,7 @@
 
 	// Pull in the main moodle config
 	// NB the following is necessary for when we pull in this config.php from a module under sloodle/mod
-	include realpath(dirname(__FILE__) . "/" . "../../config.php");
+	require_once (realpath(dirname(__FILE__) . "/" . "../../config.php"));
 
 	define('SLOODLE_WWWROOT', $CFG->wwwroot.'/mod/sloodle');
 	define('SLOODLE_DIRROOT', $CFG->dirroot.'/mod/sloodle');
