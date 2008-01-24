69ebb488bd8cca73e9dba10e72c8dd2c85c20938
pedro.mcmillan <pedro.mcmillan@a6645870-f120-0410-93b9-49b3dcf19678>
2008-01-24 15:11:50 +0000

    Added Sloodle Menu block to trunk.

---
+++ trunk/sloodle_menu/lang/en_utf8/block_sloodle_menu.php
@@ -0,0 +1,31 @@
+<?php
+// This is the English language file for the Sloodle menu block.
+// It is included automatically by the Moodle framework.
+// Retrieve strings using the Moodle get_string or print_string functions.
+
+$string['avatarname'] = 'Avatar name';
+
+$string['blockname'] = 'Sloodle Menu';
+$string['blocknameplural'] = 'Sloodle Menus';
+
+$string['loginzone'] = 'Login Zone';
+
+$string['moduleversion'] = 'Module Version';
+
+$string['nameunknown'] = 'name unknown';
+$string['noavatar'] = 'no avatar';
+$string['notecardsetuppage'] = 'Notecard Setup page';
+
+$string['objectdistributor'] = 'Object Distributor';
+
+$string['pleaselogin'] = 'Please login to use Sloodle.';
+
+$string['sloodle'] = 'Sloodle';
+$string['sloodlenotinstalled'] = 'You need to install the Sloodle activity module in order to use this block.';
+$string['sloodleconfig'] = 'Sloodle Configuration';
+$string['sloodleversion'] = 'Sloodle Version';
+
+
+$string['youravatar'] = 'Your avatar';
+
+?>
