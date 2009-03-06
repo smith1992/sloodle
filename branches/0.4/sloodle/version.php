bf299c4574450f1f8433904c99b3f46f6c11826d
firecentaur <firecentaur@a6645870-f120-0410-93b9-49b3dcf19678>
2009-03-06 07:16:16 +0000

    More fixes for the iBank

--- branches/0.4/sloodle/version.php
+++ branches/0.4/sloodle/version.php
@@ -5,7 +5,7 @@
 //  This fragment is called by /admin/index.php
 ////////////////////////////////////////////////////////////////////////////////
 
-$module->version  = 2009030414;
+$module->version  = 2009030417;
 $module->requires = 2007021500;  // Requires Moodle 1.8
 $module->cron     = 60;
 
