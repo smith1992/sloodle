4aeeea7a0f2e2e9de5e0a31334065b6d58407639
FireCentaur <FireCentaur@a6645870-f120-0410-93b9-49b3dcf19678>
2009-08-03 06:27:40 +0000

    added sloodle_awards

--- trunk/sloodle/sl_config.php
+++ trunk/sloodle/sl_config.php
@@ -91,7 +91,7 @@
     define('SLOODLE_TYPE_DISTRIB', 'distributor');
     define('SLOODLE_TYPE_PRESENTER', 'presenter');
     define('SLOODLE_TYPE_MAP', 'map');
-    define('SLOODLE_TYPE_IBANK', 'ibank');
+    define('SLOODLE_TYPE_AWARDS', 'awards');
     
     // Store the types in an array (used in lists)
     global $SLOODLE_TYPES;   
@@ -100,7 +100,7 @@
     $SLOODLE_TYPES[] = SLOODLE_TYPE_CTRL;
     $SLOODLE_TYPES[] = SLOODLE_TYPE_DISTRIB;
     $SLOODLE_TYPES[] = SLOODLE_TYPE_PRESENTER;
-    $SLOODLE_TYPES[] = SLOODLE_TYPE_IBANK; 
+    $SLOODLE_TYPES[] = SLOODLE_TYPE_AWARDS; 
     
     
     
