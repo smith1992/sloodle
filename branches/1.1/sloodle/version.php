ce45a940e555a3abe1680a36a28397297aee6d58
FireCentaur <FireCentaur@a6645870-f120-0410-93b9-49b3dcf19678>
2009-10-04 06:11:02 +0000

    will trigger creation of new tables for sloodle awards

--- branches/1.1/sloodle/version.php
+++ branches/1.1/sloodle/version.php
@@ -5,7 +5,7 @@
 //  This fragment is called by /admin/index.php
 ////////////////////////////////////////////////////////////////////////////////
 
-$module->version  = 2009072706;
+$module->version  = 2009100101;
 $module->requires = 2007021500;  // Requires Moodle 1.8
 $module->cron     = 60;
 
