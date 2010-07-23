f6d1a1f4337153a185fca7301f45b0f5abfb2ad1
pedro.mcmillan <pedro.mcmillan@a6645870-f120-0410-93b9-49b3dcf19678>
2010-07-23 15:21:45 +0000

    Removed Controller and Distributor from Add an Activity lists in SLOODLE for Schools branch.

--- sloodle/sl_config.php
+++ sloodle/sl_config.php
@@ -86,8 +86,9 @@
     // Each name should be lower-case letters only (max 50)
     // The full name should be specified in the appropriate language file, as "moduletype:type".
     
-    // Each course needs to have at least one Sloodle Access Controller before it can be accessed from in-world.
+    // In conventioanl SLOODLE, each course needs to have at least one Sloodle Access Controller before it can be accessed from in-world.
     // This is what grants access to the course as a whole, and sets prim passwords.
+    // It is not used in SLOODLE for Schools, but the identifier remains here so the code doesn't break.
     define('SLOODLE_TYPE_CTRL', 'controller');
                                    
     // These are the regular module types
@@ -98,8 +99,8 @@
     global $SLOODLE_TYPES;   
     $SLOODLE_TYPES = array();
       
-    $SLOODLE_TYPES[] = SLOODLE_TYPE_CTRL;
-    $SLOODLE_TYPES[] = SLOODLE_TYPE_DISTRIB;
+    //$SLOODLE_TYPES[] = SLOODLE_TYPE_CTRL;
+    //$SLOODLE_TYPES[] = SLOODLE_TYPE_DISTRIB;
     $SLOODLE_TYPES[] = SLOODLE_TYPE_PRESENTER;
     
     
