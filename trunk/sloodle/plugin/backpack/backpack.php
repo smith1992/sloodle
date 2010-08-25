b560872b62ddbfbede2ddc31943a020e0c9da445
FireCentaur <FireCentaur@a6645870-f120-0410-93b9-49b3dcf19678>
2010-08-25 20:43:27 +0000

    fixed auth message, was -331 for user could not access resource requested, but should have been -321

--- trunk/sloodle/plugin/backpack/backpack.php
+++ trunk/sloodle/plugin/backpack/backpack.php
@@ -26,6 +26,7 @@ class SloodleApiPluginBackpack extends SloodleApiPluginBase{
      *         
      * @package sloodle
      */
+     
      function getBalance(){
          global $CFG;
          global $sloodle;
@@ -38,8 +39,8 @@ class SloodleApiPluginBackpack extends SloodleApiPluginBase{
          $avUser->load_avatar($avuuid,$avname);
          $avUser->load_linked_user();
          $userid = $avUser->avatar_data->userid;
-          if (empty($userid)){
-           $sloodle->response->set_status_code(-331);             //line 0 - User did not have permission to access the resources requested
+         if (empty($userid)){
+           $sloodle->response->set_status_code(-321);             //line 0 - User did not have permission to access the resources requested
            $sloodle->response->set_status_descriptor('USER_AUTH'); //line 0  
            $sloodle->response->add_data_line("AVUUID:". $avuuid);    
            return; 
