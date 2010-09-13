fa04d88f61667270e0af066169dd0de205126d58
FireCentaur <FireCentaur@a6645870-f120-0410-93b9-49b3dcf19678>
2010-09-13 01:55:53 +0000

    Added getAllBalances

--- sloodle/plugin/backpack/backpack.php
+++ sloodle/plugin/backpack/backpack.php
@@ -36,6 +36,8 @@ class SloodleApiPluginBackpack extends SloodleApiPluginBase{
          $gameid=   $sloodle->request->optional_param('gameid'); 
          //build sloodle user
          $avUser = new SloodleUser( $sloodle );
+         $sloodle->validate_user();
+         $courseid = $sloodle->course->get_course_id();
          $avUser->load_avatar($avuuid,$avname);
          $avUser->load_linked_user();
          $userid = $avUser->avatar_data->userid;
@@ -52,6 +54,43 @@ class SloodleApiPluginBackpack extends SloodleApiPluginBase{
         $sloodle->response->add_data_line("AVUUID:". $avuuid);    
         $sloodle->response->add_data_line("CURRENCY:". $currency);    
         $sloodle->response->add_data_line("BALANCE:". $balance["amount"]);    
+        $sloodle->response->add_data_line("COURSEID:". $courseid);    
+     }//function
+     function getAllBalances(){
+         global $CFG;
+         global $sloodle;
+         $avuuid=   $sloodle->request->required_param('avuuid');  
+         $avname=   $sloodle->request->required_param('avname');          
+         $gameid=   $sloodle->request->optional_param('gameid'); 
+         //build sloodle user
+         $avUser = new SloodleUser( $sloodle );
+         $sloodle->validate_user();
+         $courseid = $sloodle->course->get_course_id();
+         $avUser->load_avatar($avuuid,$avname);
+         $avUser->load_linked_user();
+         $userid = $avUser->avatar_data->userid;
+         if (empty($userid)){
+           $sloodle->response->set_status_code(-321);             //line 0 - User did not have permission to access the resources requested
+           $sloodle->response->set_status_descriptor('USER_AUTH'); //line 0  
+           $sloodle->response->add_data_line("AVUUID:". $avuuid);    
+           return; 
+        }   
+        $currencies = get_records('sloodle_currency_types');
+        if ($currencies){
+           $sloodle->response->set_status_code(1);             //line 0 
+           $sloodle->response->set_status_descriptor('OK'); //line 0 
+           $sloodle->response->add_data_line("AVUUID:". $avuuid);    
+           $sloodle->response->add_data_line("COURSEID:". $courseid);    
+           $sloodle->response->add_data_line("GAMEID:". $gameid);    
+           $cObject = new SloodleCurrency();                                     
+            foreach ($currencies as $c){
+                
+                $balance = $cObject->get_balance($c->name,$userid,$avuuid,$gameid);
+                if ($balance["amount"]=="")$b=0; else $b=$balance["amount"];
+                $sloodle->response->add_data_line($c->name."|".$b);         
+            }            
+        }         
+        
      }//function
 }//class
 ?>
